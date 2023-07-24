c66252a19e3f17e0802c26630a3397111629b626
Mr. Mendez <mrmendez@amvestor.com>
2023-07-24 16:13:54 -0400

    fixinx CI release

--- setup.py
+++ setup.py
@@ -3,8 +3,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='PyFaudit',
-    version='0.1',
+    version='0.1.0',
     packages=find_packages(),
+    long_description_content_type='text/markdown',
+    long_description=open('README.md').read(),
     entry_points={
         'console_scripts': [
             'PyFaudit = PyFaudit.scanner:main',
